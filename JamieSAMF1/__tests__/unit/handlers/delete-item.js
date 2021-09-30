9b16f295e7e6475e5898cb7b6cea86081982dda2
EC2 Default User <ec2-user@ip-172-31-20-52.eu-west-1.compute.internal>
2021-09-30 09:00:18 +0000

    altered delete function

---
+++ JamieSAMF1/__tests__/unit/handlers/delete-item.js
@@ -0,0 +1,43 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.TABLE_NAME;
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+exports.deleteItemHandler = async (event) => {
+    if (event.httpMethod !== 'DELETE') {
+        throw new Error(`postMethod only accepts DELETE method, you tried: ${event.httpMethod} method.`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Get id and name from the body of the request
+    //const body = JSON.parse(event.body)
+    const Year = event.pathParameters.Year;
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+     let params = {
+    TableName : tableName,
+    Key: { Year : Year },
+  };
+  const data = await docClient.delete(params).promise();
+  //const item = data.Item;
+
+    //const result = await docClient.put(params).promise();
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify("Driver Deleted")
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
